11b1a82d0a8a53c31586ee593add7b768ea4ee14
dajacobs <dajacobs.tab@gmail.com>
2015-09-08 09:27:35 -0500

    main func loop

--- tools/net.py
+++ tools/net.py
@@ -48,3 +48,21 @@ def main():
     except getopt.GetoptError as err:
         print str(err)
         usage()
+
+    for o,a in opts:
+        if o in ("-h", "--help"):
+            usage()
+        elif o in ("-l", "--listen"):
+            listen = True
+        elif o in ("-e", "--execute"):
+            execute = a
+        elif o in ("-c", "--commandshell"):
+            command = True
+        elif o in ("-u", "--upload"):
+            upload_destination = a
+        elif o in ("-t", "--target"):
+            target = a
+        elif o in ("-p", "--port"):
+            port = int(a)
+        else:
+            assert False, "Unhandled Option"

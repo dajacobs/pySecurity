0b92a913916db0f60cae5d5f5c8293359e091102
dajacobs <dajacobs.tab@gmail.com>
2015-10-13 19:14:48 -0500

    update

--- tools/bh_sshRCmd.py
+++ tools/bh_sshRCmd.py
@@ -5,3 +5,20 @@ import paramiko
 import subprocess
 
 def ssh_command(ip, user, passwd, command):
+	client = paramiko.SSHClient()
+	client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+	client.connect(ip, username=user, password=passwd)
+	ssh_session = client.get_transport().open_session()
+	if ssh_session.active:
+		ssh_session.send(command)
+		print ssh_session.recv(1024)
+		while True:
+			command = ssh_session.recv(1024)
+			server
+			try:
+				cmd_output = subprocess.check_output(command, shell=True)
+				ssh_session.send(cmd_output)
+			except Exception,e:
+				ssh_session.send(str(e))
+		client.close()
+	return			

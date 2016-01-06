176faa717e36f1ed0050f46c58d5c521057b70fd
dajacobs <dajacobs.tab@gmail.com>
2016-01-05 23:34:11 -0600

    handler func

--- paramiko/rforward.py
+++ paramiko/rforward.py
@@ -31,3 +31,22 @@ def reverse_forward_tunnel(server_port, remote_host, remote_port, transport):
 		thr = threading.Thread(target=handler, args=(chan, remote_host, remote_port))
 		thr.setDaemon(True)
 		thr.start()
+def handler(chan, host, port):
+	sock = socket.socket()
+	try:
+		sock.connect((host, port))
+	except Exception as e:
+		verbose('Forwarding request to %s:%d failed: %r' % (host, port, e))
+		return
+	verbose('Connected! Tunnel open %r -> %r -> %r' % (chan.origin_addr, chan.getpeername(), (host, port)))
+	
+	while True:
+		r, w, x = select.select([sock, chan], [], [])
+		if sock in r:
+			data = sock.recv(1024)
+			if len(data) == 0:
+				break
+			sock.send(data)
+	chan.close()
+	sock.close()
+	verbose('Tunnel closed from %r' % (chan.origin_addr,))							
